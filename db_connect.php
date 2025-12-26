e49c1662b6feba8f43231acd03ea0b8da48bf0fa
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2025-12-26 17:01:35 +0545

    Move all files into code folder and remove old root files

--- db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "student_feedback_system");
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-?>     
